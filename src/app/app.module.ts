f319f975030446a4f3e22a8a00f06360833850ba
mevshnu <vishnusivan1000@gmail.com>
2022-11-24 23:48:04 +0530

    lal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { ViewComponent } from './view/view.component';
+import { SearchComponent } from './search/search.component';
+import { DeleteComponent } from './delete/delete.component';
+import { BookEntryComponent } from './book-entry/book-entry.component';
+import { EditComponent } from './edit/edit.component';
+import { IssueComponent } from './issue/issue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminloginComponent
+    AdminloginComponent,
+    UserLoginComponent,
+    UserSignupComponent,
+    ViewComponent,
+    SearchComponent,
+    DeleteComponent,
+    BookEntryComponent,
+    EditComponent,
+    IssueComponent
   ],
   imports: [
     BrowserModule,
