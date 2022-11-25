a35eec7033329ae70a009a345b3ecec3dfc2c075
mevshnu <vishnusivan1000@gmail.com>
2022-11-25 11:30:41 +0530

    lol

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent {
-
+books:any =[
+  {
+    "title":"It Starts With Us",
+    "image":"https://m.media-amazon.com/images/I/51W-r3VQlAL._SX320_BO1,204,203,200_.jpg",
+    "author": "Colleen Hoover",
+    "publisher":"Simon & Schuster",
+    "language":"English",
+    "price":200
+  },
+  {
+    "title":"It Starts With Us",
+    "image":"https://m.media-amazon.com/images/I/51W-r3VQlAL._SX320_BO1,204,203,200_.jpg",
+    "author": "Colleen Hoover",
+    "publisher":"Simon & Schuster",
+    "language":"English",
+    "price":200
+  }
+]
 }
