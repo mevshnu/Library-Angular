7da0858f5a18c5b8079382478a4009d100f11409
mevshnu <vishnusivan1000@gmail.com>
2022-11-28 15:25:11 +0530

    poppz

--- src/app/user-signup/user-signup.component.ts
+++ src/app/user-signup/user-signup.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 export class UserSignupComponent {
 name=""
-adharNumber
+adharNumber=""
+address=""
+pinCode=""
+dob=""
+email=""
+phone=""
+userName=""
+password=""
+cpassword=""
+readSignup=()=>
+{
+  let data5:any={
+"name":this.name,
+"adharNumber":this.adharNumber,
+"address":this.address,
+"pinCode":this.pinCode,
+"dob":this.dob,
+"email":this.email,
+"phone":this.phone,
+"userName":this.userName,
+"password":this.password,
+"cpassword":this.cpassword
+  }
+  console.log(data5)
+}
+
 }
