b8b4bd895f9dd3c6a91c5de9a6240870ae7ffb1c
mevshnu <vishnusivan1000@gmail.com>
2022-11-30 10:55:08 +0530

    poi

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -8,10 +9,19 @@ import { Component } from '@angular/core';
 export class UserLoginComponent {
   user=""
   password=""
+  constructor(private route:Router){}
   readLogin=()=>
   {
     let data4:any={"user":this.user,"password":this.password}
   console.log(data4)
+  if(this.user=="user" && this.password=="12345")
+  {
+    this.route.navigate(["/view"])
+  }
+  else
+  {
+alert("invalid login")
+  }
   }
 
 }
